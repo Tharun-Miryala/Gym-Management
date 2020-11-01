a036b40a9d6d1101b5ab1a704d66df8e4231c774
Tharun-Miryala <54226096+Tharun-Miryala@users.noreply.github.com>
2020-11-01 18:20:47 +0530

    Add files via upload

---
+++ mainly.py
@@ -0,0 +1,140 @@
+import os
+from subprocess import call
+
+import sys
+
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
+
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+def click_checkinn():
+    call(["python", "checkin_gui_and_program.py"])
+def click_list():
+    call(["python", "listgui.py"])
+def click_getinfo():
+    call(["python","getinfoui.py"])
+
+
+class GYM_MANAGEMENT:
+    def __init__(self):
+        root = Tk()
+        '''This class configures and populates the toplevel window.
+           top is the toplevel containing window.'''
+        _bgcolor = '#d9d9d9'  # X11 color: 'gray85'
+        _fgcolor = '#000000'  # X11 color: 'black'
+        _compcolor = '#ffffff' # X11 color: 'white'
+        _ana1color = '#ffffff' # X11 color: 'white'
+        _ana2color = '#ffffff' # X11 color: 'white'
+        font14 = "-family {Segoe UI} -size 15 -weight bold -slant "  \
+            "roman -underline 0 -overstrike 0"
+        font16 = "-family {Swis721 BlkCn BT} -size 40 -weight bold "  \
+            "-slant roman -underline 0 -overstrike 0"
+        font9 = "-family {Segoe UI} -size 9 -weight normal -slant "  \
+            "roman -underline 0 -overstrike 0"
+
+        root.geometry("963x749+540+110")
+        root.title("GYM MANAGEMENT")
+        root.configure(background="#d9d9d9")
+        root.configure(highlightbackground="#d9d9d9")
+        root.configure(highlightcolor="black")
+
+
+
+        self.menubar = Menu(root,font=font9,bg=_bgcolor,fg=_fgcolor)
+        root.configure(menu = self.menubar)
+
+
+
+        self.Frame1 = Frame(root)
+        self.Frame1.place(relx=0.02, rely=0.03, relheight=0.94, relwidth=0.96)
+        self.Frame1.configure(relief=GROOVE)
+        self.Frame1.configure(borderwidth="2")
+        self.Frame1.configure(relief=GROOVE)
+        self.Frame1.configure(background="#d9d9d9")
+        self.Frame1.configure(highlightbackground="#d9d9d9")
+        self.Frame1.configure(highlightcolor="black")
+        self.Frame1.configure(width=925)
+
+        self.Message6 = Message(self.Frame1)
+        self.Message6.place(relx=0.09, rely=0.01, relheight=0.15, relwidth=0.86)
+        self.Message6.configure(background="#ff6347")
+        self.Message6.configure(font=font16)
+        self.Message6.configure(foreground="#000000")
+        self.Message6.configure(highlightbackground="#ff6347")
+        self.Message6.configure(highlightcolor="black")
+        self.Message6.configure(text='''WELCOME''')
+        self.Message6.configure(width=791)
+
+        self.Button2 = Button(self.Frame1)
+        self.Button2.place(relx=0.18, rely=0.17, height=103, width=566)
+        self.Button2.configure(activebackground="#d9d9d9")
+        self.Button2.configure(activeforeground="#000000")
+        self.Button2.configure(background="#d9d9d9")
+        self.Button2.configure(disabledforeground="#bfbfbf")
+        self.Button2.configure(font=font14)
+        self.Button2.configure(foreground="#000000")
+        self.Button2.configure(highlightbackground="#FFFFFF")
+        self.Button2.configure(highlightcolor="white")
+        self.Button2.configure(pady="0")
+        self.Button2.configure(text='''1.CUSTOMER''')
+        self.Button2.configure(width=566)
+        self.Button2.configure(command=click_checkinn)
+
+        self.Button3 = Button(self.Frame1)
+        self.Button3.place(relx=0.18, rely=0.34, height=93, width=566)
+        self.Button3.configure(activebackground="#d9d9d9")
+        self.Button3.configure(activeforeground="#000000")
+        self.Button3.configure(background="#d9d9d9")
+        self.Button3.configure(disabledforeground="#bfbfbf")
+        self.Button3.configure(font=font14)
+        self.Button3.configure(foreground="#000000")
+        self.Button3.configure(highlightbackground="#d9d9d9")
+        self.Button3.configure(highlightcolor="black")
+        self.Button3.configure(pady="0")
+        self.Button3.configure(text='''2.SHOW CUSTOMER LIST''')
+        self.Button3.configure(width=566)
+        self.Button3.configure(command=click_list)
+
+        self.Button4 = Button(self.Frame1)
+        self.Button4.place(relx=0.18, rely=0.50, height=103, width=566)
+        self.Button4.configure(activebackground="#d9d9d9")
+        self.Button4.configure(activeforeground="#000000")
+        self.Button4.configure(background="#d9d9d9")
+        self.Button4.configure(disabledforeground="#bfbfbf")
+        self.Button4.configure(font=font14)
+        self.Button4.configure(foreground="#000000")
+        self.Button4.configure(highlightbackground="#d9d9d9")
+        self.Button4.configure(highlightcolor="black")
+        self.Button4.configure(pady="0")
+        self.Button4.configure(text='''3.GET INFO OF CUSTOMER PACKAGE''')
+        self.Button4.configure(width=566)
+        self.Button4.configure(command=click_getinfo)
+
+        self.Button5 = Button(self.Frame1)
+        self.Button5.place(relx=0.18, rely=0.69, height=103, width=566)
+        self.Button5.configure(activebackground="#d9d9d9")
+        self.Button5.configure(activeforeground="#000000")
+        self.Button5.configure(background="#d9d9d9")
+        self.Button5.configure(disabledforeground="#bfbfbf")
+        self.Button5.configure(font=font14)
+        self.Button5.configure(foreground="#000000")
+        self.Button5.configure(highlightbackground="#d9d9d9")
+        self.Button5.configure(highlightcolor="black")
+        self.Button5.configure(pady="0")
+        self.Button5.configure(text='''4.EXIT''')
+        self.Button5.configure(width=566)
+        self.Button5.configure(command=quit)
+        root.mainloop()
+
+
+if __name__ == '__main__':
+    GUUEST=GYM_MANAGEMENT()
+
+
